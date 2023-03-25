9e6ccdb5336c5b8b8ab7847b1df4c8371360ac3d
Orphan-Crippler <6201093+Orphan-Crippler@users.noreply.github.com>
2023-03-25 19:13:37 -0400

    editing my keymap

--- keyboards/xiudi/xd75/keymaps/orphancrippler/keymap.c
+++ keyboards/xiudi/xd75/keymaps/orphancrippler/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, KC_KP_7, KC_KP_8, KC_KP_9,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_KP_4, KC_KP_5, KC_KP_6,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_KP_1, KC_KP_2, KC_KP_3,
-        KC_LGUI, KC_APP,  KC_LALT, KC_LCTL, KC_LO,   KC_SPC,   KC_SPC,  KC_HI,   KC_RCTL, KC_RALT, KC_APP,  KC_RGUI, KC_KP_1, KC_PDOT, KC_ENT
+        KC_LGUI, KC_APP,  KC_LALT, KC_LCTL, KC_LO,   KC_SPC,   KC_SPC,  KC_HI,   KC_RCTL, KC_RALT, KC_APP,  KC_RGUI, KC_KP_0, KC_PDOT, KC_ENT
     ),
 
     [_LO] = LAYOUT_ortho_5x15( /* Lower */
